5a3a3b34e90042f1a4993d63c58d9c8b142fc21d
Brandon Dixon <bdixon@cs.uchicago.edu>
2016-05-31 14:23:36 -0500

    jsdf

--- Scripts/clusterer.py
+++ Scripts/clusterer.py
@@ -8,31 +8,55 @@ from StringIO import StringIO
 sc = pyspark.SparkContext()
 
 
-def parser(x):
-
-    rv = list(csv.reader(x, delimiter=","))
+def to_float(x):
 
     i = 0
 
-    for data in rv:
+    for col in x:
         try:
-            data = float(data)
+            col = float(col)
         except:
-            data = 0
-    
-        rv[i] = data
+            col = 0
+
+        x[i] = col
         i = i + 1
 
-    return rv
+def csv_parser(line):
+
+    try:
+        rv = list(csv.reader(StringIO(line), delimiter=","))[0]
+        rv = rv[6:175] + rv[179:]
+        if((rv == None) or (rv[2] == "NA")):
+            rv = [1]
+        try:
+            rv = [int(i) for i in rv]
+        except:
+            rv = [1]
+        return rv
+    except:
+        rv = [1]  #This row will be removed in data cleaning
+        return rv
+
+def primary_data_cleaning(file_path):
+
+    lines = sc.textFile(file_path)
+    header = lines.first()
+    rm = lines.filter(lambda x: x != header) # remove first line
+
+    data = rm.map(csv_parser)
+    data = data.filter(lambda x: len(x) != 1)
+    data.saveAsTextFile("gs://cs123data/Output/merge.txt")
+
+    data = data.map(to_float)
+
+    return data
 
 
 #from : http://spark.apache.org/docs/latest/mllib-clustering.html
 # Load and parse the data
-data = sc.textFile("gs://cs123data/Data/updated_merger_3.csv")
-parsedData = data.map(parser)
-
+data = primary_data_cleaning("gs://cs123data/Data/updated_merger_3.csv")
 # Build the model (cluster the data)
-clusters = KMeans.train(parsedData, 10, maxIterations=10,
+clusters = KMeans.train(data, 10, maxIterations=10,
         runs=10, initializationMode="random")
 
 # Evaluate clustering by computing Within Set Sum of Squared Errors
