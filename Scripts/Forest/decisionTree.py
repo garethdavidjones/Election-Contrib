5a5e4930a3108dc2f2042d5acb7efffadd3ca7ef
Gareth Jones <garethjones@cs.uchicago.edu>
2016-06-01 21:52:34 -0500

    Vecotrizer working

--- Scripts/Forest/decisionTree.py
+++ Scripts/Forest/decisionTree.py
@@ -2,28 +2,35 @@
 import pyspark
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import RandomForest
-from pyspark.mllib.util.MLUtils import loadLabeledPoints 
-
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
 
 def main(input_file):
 
     sc = pyspark.SparkContext(appName="DecisionTree")
 
-    data = loadLabeledPoints(sc, input_file)
+    data = MLUtils.loadLabeledPoints(sc, input_file)
 
-    trainingData, testData, CVdata = formated.randomSplit([0.70, 0.15, 0.15])
+    trainingData, testData, CVdata = data.randomSplit([0.70, 0.15, 0.15])
     # Cache in memory for faster training
-    traniningData.cache()
+    trainingData.cache()
     testData.cache()
     CVdata.cache()
+    bar = trainingData.first()
+    num_features = len(bar.features) - 4
+    base_feature_count = 9
+    features = {3:3, 4:7, 5:4, 6:2, 7:3, 8:3}
+    
+    # for i in range(base_feature_count + 1, num_features + base_feature_count + 1):
+    #     features[i] = 10
 
-    tree_model = DecisionTree.trainClassifier(training_data, numClasses=3, impurity='gini', maxDepth=16, maxBins=10)
+    tree_model = DecisionTree.trainClassifier(trainingData, numClasses=3, impurity='gini',
+                 categoricalFeaturesInfo=features, maxDepth=4, maxBins=10)
 
+    predictions = tree_model.predict(testData.map(lambda x: x.features))
+    labels_and_preds = testData.map(lambda x: x.label).zip(predictions)    
 
-    predictions = tree_model.predict(test_data.map(lambda x: x.features))
-    labels_and_preds = test_data.map(lambda x: x.label).zip(predictions)    
-
-    test_accuracy = labels_and_preds.filter(lambda (y, x): y == x).count() / float(test_data.count())
+    test_accuracy = labels_and_preds.filter(lambda (y, x): y == x).count() / float(testData.count())
 
     print tree_model.toDebugString()
     print "Test accuracy: {}".format(round(test_accuracy,4))
@@ -32,5 +39,5 @@ def main(input_file):
 if __name__ == '__main__':
    
 
-    input_file = "gs://cs123data/"
+    input_file = "gs://cs123data/Output/LabeledVectors3/"
     main(input_file)
