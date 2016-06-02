0d229c166e62ab53b75ba86411f5f967719d627e
Brandon Dixon <Brandon Dixon>
2016-06-01 22:56:00 -0500

    Brandon's attempt. Watch it fail.

---
+++ Scripts/Forest/random_forest.py
@@ -0,0 +1,48 @@
+import pyspark
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.ml import Pipeline
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.feature import VectorIndexer
+from pyspark.ml.evaluation import RegressionEvaluator
+
+    
+def main(input_file):
+    # Load and parse the data file, converting it to a DataFrame.
+    data = MLUtils.loadLabeledPoints(sc, input_file)
+
+    # Automatically identify categorical features, and index them.
+    # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    featureIndexer =\
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=10).fit(data)
+
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data.randomSplit([0.7, 0.3])
+
+    # Train a RandomForest model.
+    rf = RandomForestRegressor(featuresCol="indexedFeatures")
+
+    # Chain indexer and forest in a Pipeline
+    pipeline = Pipeline(stages=[featureIndexer, rf])
+
+    # Train model.  This also runs the indexer.
+    model = pipeline.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+
+    # Select example rows to display.
+    predictions.select("prediction", "label", "features").show(5)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = RegressionEvaluator(
+        labelCol="label", predictionCol="prediction", metricName="rmse")
+    rmse = evaluator.evaluate(predictions)
+    print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+
+    rfModel = model.stages[1]
+    print(rfModel)  # summary only
+
+if __name__ == '__main__':
+	
+	input_file = "gs://cs123data/Output/LabeledVectors3/"
+    main(input_file)
